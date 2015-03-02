2b93719b39027945a2cc8a5d2bfb331b2ca0ab1a
Kevin Kemp <kmkemp11@gmail.com>
2015-03-02 16:06:27 -0600

    removed old project

--- SharedWebComp/Models/TestViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SharedWebComponents.Models {
-    public class TestViewModel {
-        public TestViewModel(string greeting) {
-            Greeting = greeting;
-        }
-
-        public string Greeting { get; set; }
-    }
-}
